68fc41408f07c82392bb18d8934bdeda53391432
yannickict <yblatty@edp.ch>
2025-03-14 10:21:11 +0100

    added tests

--- src/rounding.test.ts
+++ src/rounding.test.ts
@@ -3,3 +3,11 @@ import { roundTo } from "./rounding";
 test("check round to nickels", () => {
   expect(roundTo(10.0 / 3.0, 0.05)).toBe(3.33);
 });
+
+test("check round to nickels", () => {
+  expect(roundTo(10.0 / 2.0, 0.05)).toBe(5);
+});
+
+test("check round to nickels", () => {
+  expect(roundTo(10.0 / 1, 0.05)).toBe(10);
+});
